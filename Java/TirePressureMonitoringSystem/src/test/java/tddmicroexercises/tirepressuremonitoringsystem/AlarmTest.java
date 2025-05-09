0e9749dbd8c27afd1fa2055a1add073159e85893
Gracious Mashasha <gracious.mashasha@gmail.com>
2025-05-09 15:35:09 +0200

    TirePressureMonitoringSystem exercise Test

--- Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
+++ Java/TirePressureMonitoringSystem/src/test/java/tddmicroexercises/tirepressuremonitoringsystem/AlarmTest.java
@@ -1,14 +1,46 @@
 package tddmicroexercises.tirepressuremonitoringsystem;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-
+import static org.mockito.Mockito.*;
 public class AlarmTest {
 
     @Test
     public void foo() {
-        Alarm alarm = new Alarm();
+        
+        Alarm alarm = new Alarm(null);
         assertFalse(alarm.isAlarmOn());
+    }
+
+    @Test
+    public void alarmShouldTurnOnWhenLow(){
+        Sensor sensor = mock(Sensor.class);
+        when(sensor.popNextPressurePsiValue()).thenReturn(16.0);
+
+        Alarm alarm = new Alarm(sensor);
+        alarm.check();
+        assertTrue( alarm.alarmOn );
+    }
+
+    @Test
+    public void alarmShouldTurnOnWhenHigh(){
+        Sensor sensor = mock(Sensor.class);
+        when(sensor.popNextPressurePsiValue()).thenReturn(22.0);
+
+        Alarm alarm = new Alarm(sensor);
+        alarm.check();
+        assertTrue( alarm.alarmOn );
+    }
+
+    @Test
+    public void alarmShouldBeOffWhenInRange(){
+        Sensor sensor = mock(Sensor.class);
+        when(sensor.popNextPressurePsiValue()).thenReturn(20.0);
+
+        Alarm alarm = new Alarm(sensor);
+        alarm.check();
+        assertFalse( alarm.alarmOn );
     }
 }
